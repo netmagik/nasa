206afe0209e62a2d4668ee8f5543365eb9fc8553
Irina <irina@netmagik.com>
2022-12-15 12:52:29 -0500

    Add content

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/style.css")
+    eleventyConfig.addPassthroughCopy("src/images/")
+    eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.setTemplateFormats("html,njk");
+  
+    // Input directory: src
+    // Output directory: _site
+    return {
+      dir: {
+        input: "src"
+      }
+    }
+  }
